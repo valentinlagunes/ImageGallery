01cdb3e018f63d985ef45b8386f0d1bd2368f345
Valentin <isaac.lagunes@yahoo.com>
2020-08-08 16:07:48 -0700

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Isaac on 8/8/20.
+//  Copyright © 2020 ValentinLagunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
